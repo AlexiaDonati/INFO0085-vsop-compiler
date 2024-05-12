577d666a41fa144c4280dc256c76936fe925ac97
Pagano Florian <florian.pagano@hotmail.be>
2024-05-12 14:02:49 +0200

    creation of code generation visitor

---
+++ part4/code_generation_visitor.cpp
@@ -0,0 +1,106 @@
+#include "ast.hpp"
+#include "code_generation_visitor.hpp"
+#include <iostream>
+
+using namespace AST;
+using namespace std;
+
+void* Code_generation_visitor::visit(Program* program){
+    program->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Class* class_){
+    class_->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Field* field){
+    field->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Method* method){
+    method->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Formal* formal){
+    formal->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Block* block){
+    block->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(If* if_){
+    if_->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(While* while_){
+    while_->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Let* let){
+    let->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Assign* assign){
+    assign->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Self* self){
+    self->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Unop* unop){
+    unop->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Binop* binop){
+    binop->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Call* call){
+    call->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(New* new_){
+    new_->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(String* string_){
+    string_->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Integer* integer){
+    integer->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Boolean* boolean){
+    boolean->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Unit* unit){
+    unit->get_column();
+    return NULL;
+}
+
+void* Code_generation_visitor::visit(Object* object){
+    object->get_column();
+    return NULL;
+}
